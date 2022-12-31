5b706e78c1ad5e2d7cdd87c076dfc3480d093713
Ravi-Rathod-22 <raviarthod4449@gmail.com>
2023-01-01 00:14:00 +0530

    Add SideBar

---
+++ src/components/SideBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-navigation-drawer expand-on-hover rail>
+    <v-list>
+      <v-list-item
+        prepend-avatar="https://randomuser.me/api/portraits/lego/5.jpg"
+        title="Ravi Rathod"
+        subtitle="ravirathod4449@gmail.com"
+      ></v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-list-item
+        prepend-icon="mdi-image"
+        title="Color Gallery"
+        value="color"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-dialpad"
+        title="GreySpace Gallery"
+        value="greyspace"
+      ></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
