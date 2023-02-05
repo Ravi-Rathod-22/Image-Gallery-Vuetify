0f3fb996a84cdd33decb4adcefa3a664d2b0ba24
Ravi-Rathod-22 <raviarthod4449@gmail.com>
2023-02-05 16:21:55 +0530

    add some css classes

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,21 +1,24 @@
 <template>
+  <v-card class="mx-5 my-2 pa-3" >
     <v-row>
-        <v-col v-for="n in 200"  :key="n" cols="1" sm="3" md="2" lg="1">
-            <v-card>
-                <v-img :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`" :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`" aspect-ratio="1" cover>
-                 <template v-slot:placeholder>
-          <v-row
-            class="fill-height ma-0"
-            align="center"
-            justify="center"
+      <v-col v-for="n in 200" :key="n" cols="1" sm="3" md="2" lg="1">
+        <v-card>
+          <v-img
+            :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+            :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+            aspect-ratio="1"
+            cover
           >
-            <v-progress-circular
-              indeterminate
-              color="grey lighten-5"
-            ></v-progress-circular>
-          </v-row>
-        </template></v-img>
-            </v-card>
-        </v-col>
+            <template v-slot:placeholder>
+              <v-row class="fill-height ma-0" align="center" justify="center">
+                <v-progress-circular
+                  indeterminate
+                  color="grey lighten-5"
+                ></v-progress-circular>
+              </v-row> </template
+          ></v-img>
+        </v-card>
+      </v-col>
     </v-row>
+  </v-card>
 </template>
